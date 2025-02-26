5a2b4c7dc3d2338ba8d229f24ee6b48f304553dd
lucas serra <lucasserradevelopment@gmail.com>
2025-02-26 19:39:57 -0300

    Initial commit

---
+++ src/main/kotlin/lucasserra/dev/social_media_api/SocialMediaApiApplication.kt
@@ -0,0 +1,11 @@
+package lucasserra.dev.social_media_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMediaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialMediaApiApplication>(*args)
+}
---
+++ src/test/kotlin/lucasserra/dev/social_media_api/SocialMediaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package lucasserra.dev.social_media_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
